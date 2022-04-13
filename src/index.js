b0e028b5027fc9060e622c8dc4ab8de171bf5cbb
Александр Осипов <aosipv@yandex.ru>
2022-04-13 23:09:27 +0400

    add clients user model

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import Fastify from "fastify";
+import fastifyCors from "fastify-cors";
 import TelegramBot from "node-telegram-bot-api";
 import dotenv from "dotenv";
 import { DB } from "./db.js";
-import { UserModel } from "./models.js";
+import { UserModel, ClientModel } from "./models.js";
 
 dotenv.config();
+
+const passWord = process.env.PASS_WORD;
 
 const bot = new TelegramBot(process.env.BOT_API, { polling: true });
 
@@ -12,22 +15,32 @@ const startBot = async () => {
   try {
     await DB.authenticate();
     await DB.sync();
-    console.log("auth");
   } catch (e) {
     console.log("Подключение к бд сломалось", e);
   }
 
   bot.on("message", async (msg) => {
     const chatId = msg.chat.id;
-    try {
-      const user = await UserModel.findOne({ chatId });
-      console.log(user);
+
+    if ((msg.text === "/start") | (msg.text === "/auth")) {
+      const user = await UserModel.findOne({ where: { chatId: `${chatId}` } });
+
       if (user) {
-        return bot.sendMessage(chatId, `Не нужно писать /start еще раз!`);
+        return bot.sendMessage(chatId, `Не нужно писать ${msg.text} еще раз!`);
       }
+
       await UserModel.create({ chatId });
-      return bot.sendMessage(chatId, `Скоро сюда будут поступать заявки!`);
-    } catch (e) {}
+
+      return bot.sendMessage(
+        chatId,
+        `Введите кодовое слово для подписки на уведомления`
+      );
+    } else if (msg.text === passWord) {
+      return bot.sendMessage(
+        chatId,
+        `Готово! Теперь вы получаете уведомления!`
+      );
+    }
   });
 };
 
@@ -35,14 +48,30 @@ startBot();
 
 const fastify = Fastify();
 
+fastify.register(fastifyCors);
+
 fastify.post("/callback", async (request, reply) => {
   try {
+    const { phone, type, fault, city } = request.body;
+
+    const client = await ClientModel.findOne({ where: { phone } });
+    if (client) return reply.code(400).send();
+
+    await ClientModel.create({ phone });
+
     const users = await UserModel.findAll();
     for (const user of users) {
-      bot.sendMessage(user.chatId, JSON.stringify(request.body));
+      bot.sendMessage(
+        user.chatId,
+        `*Телефон:* \`${phone}\`\n${type ? `*Тип техники:* ${type}` : ""}\n${
+          fault ? `*Неисправность:* ${fault}` : ""
+        }\n${city ? `*Город:* ${city}` : ""}`,
+        { parse_mode: "Markdown" }
+      );
     }
   } catch (error) {}
-  reply.code(200).send("");
+
+  reply.code(200).send();
 });
 
 fastify.listen(3000, function (err) {
--- src/models.js
+++ src/models.js
@@ -10,3 +10,16 @@ export const UserModel = DB.define("user", {
   },
   chatId: { type: DataTypes.STRING, unique: true },
 });
+
+export const ClientModel = DB.define("client", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    unique: true,
+    autoIncrement: true,
+  },
+  phone: { type: DataTypes.STRING, unique: true },
+  type: { type: DataTypes.STRING, unique: true },
+  fault: { type: DataTypes.STRING, unique: true },
+  city: { type: DataTypes.STRING, unique: true },
+});
